83cd32828150fd0cd17fb042c1c3a12c01f7d9c5
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-22 19:21:12 -0600

    setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import React from 'react';
 import './App.css';
 import QuizScreen from './screens/QuizScreen';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
-  return <QuizScreen />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <HomeScreen />
+        </Route>
+        <Route exact path='/quiz'>
+          <QuizScreen />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;

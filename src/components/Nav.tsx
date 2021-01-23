cc3dbdcf0cc4c0b08742668ab621d0007bc8c934
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-22 19:36:31 -0600

    style nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -16,8 +16,28 @@ export default function Nav() {
   );
 }
 
-const NavContainer = styled.nav``;
+const NavContainer = styled.nav`
+  background: #222;
+  padding: 1rem;
+  box-shadow: 2px 2px 10px #6d6d6d;
+`;
 
-const NavListContainer = styled.ul``;
+const NavListContainer = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 2rem;
+`;
 
-const NavItem = styled.li``;
+const NavItem = styled.li`
+  text-decoration: none;
+  font-size: 2rem;
+  color: #8a8a8a;
+  padding: 0 2rem;
+  transition: all 0.2s ease;
+
+  &:hover {
+    cursor: pointer;
+    color: white;
+  }
+`;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,9 @@
+import Nav from '../components/Nav';
+
 export default function HomeScreen() {
   return (
     <>
+      <Nav />
       <h1>Home</h1>
     </>
   );
